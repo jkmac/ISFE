742a3dd10436eef6d852ff82eff3d164ce7dc673
jkmac <infinspac@gmail.com>
2014-06-07 09:52:40 -0700

    lsqfit update

--- lsqfit.m
+++ lsqfit.m
@@ -4,19 +4,16 @@ function [x,x0,resnorm,residual] = lsqfit(sq, lbt, acu, mean, std)
 	ydata = sq(:,2)./xdata;
 	%define optimization parameters
 	global r
-% 	if lbt == 0aaaaa
-% 	    lb = [];
-% 	else
-% 	    lb = lbt*ones(length(r)+2,1);
-% 	end
 	ub = cat(1,abs(r), [inf;inf]);
     lb = -ub;
 	options = optimset('TolFun',10^acu,'MaxFunEvals',150000);
 	%random initialization of rms (x0)
 	x0 = random('Normal', mean, std, length(r)+2, 1);
 	%call the routine
-    x0(end-1) = 1;
+    x0(end-1) = 3;
     x0(end) = 0;
+    lowest = 0.1;
+    lb(end-1) = lowest;
     flag=glcheck(x0, xdata);
 	[x,resnorm,residual] = lsqcurvefit(@myfit1, x0, xdata, ydata, lb, ub, options);
 end
